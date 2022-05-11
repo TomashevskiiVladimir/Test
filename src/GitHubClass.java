3398ac3da0f3468b0b68ab3c3c8e30dab383e8b4
VergunVira <samarskavira@gmail.com>
2022-05-11 19:40:08 -0400

    hey buddy

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -3,6 +3,10 @@ public class GitHubClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 10;
+		int c = a+b;
+		System.out.println(c);
 
 	}
 
